f4618cf2e757c1688b1a22e90e1b45376fd3879a
ismailcubuk <iismailcubuk@gmail.com>
2023-03-20 01:06:16 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState,useEffect } from "react";
 
 function App() {
+  const [weather, setWeather] = useState()
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+
+  
+  const getWeatherData = async (lat, lon) => {
+    const key = "f816f1c7fc58061a8d4b99d210789fa3";
+    try{
+      const { data } = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`
+        );
+      setWeather(data)
+    } catch {
+      alert("Can't get data")
+    }
+  };
+
+    useEffect(() => {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLatitude(position.coords.latitude);
+          setLongitude(position.coords.longitude);
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+      longitude && latitude && getWeatherData(longitude,latitude)
+    }, [longitude,latitude]);
+
+console.log(weather,longitude,latitude);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+      long = {longitude}
+      </div>
+      <div>
+      lat = {latitude}
+      </div>
     </div>
   );
 }
