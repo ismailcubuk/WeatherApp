1958c32c6b51afcc0c7b15aa36d18cfad19dfa5e
ismailcubuk <iismailcubuk@gmail.com>
2023-03-22 08:24:00 +0300

    build body commit

---
+++ src/components/forecast/Forecast.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Forecast() {
+  return (
+    <div className='flex flex-col w-5/12'>
+      <div className='border-blue-700 border-2 h-4/6'>
+        img
+      </div>
+      <div className='border-red-500 border-2 h-2/6'>
+        <p>Today's forecast for Amsterdam,Netherlaneds</p>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Forecast
