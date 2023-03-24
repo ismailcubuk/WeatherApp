748e9d91b31bc64448af6e2fda365eea89d513cb
ismailcubuk <iismailcubuk@gmail.com>
2023-03-24 09:52:32 +0300

    Fetch Icons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+
 import { useState, useEffect } from "react";
 import CurrentWeather from "./components/current-weather/CurrentWeather";
 import WeatherDetail from "./components/current-weather/WeatherDetail";
@@ -6,37 +6,14 @@ import Forecast from "./components/forecast/Forecast";
 import Navbar from "./components/navbar/Navbar";
 
 function App() {
-  const [weather, setWeather] = useState()
-  const [latitude, setLatitude] = useState(null);
-  const [longitude, setLongitude] = useState(null);
+ 
+
+  
 
 
-  // const getWeatherData = async (lat, lon) => {
-  //   const key = "f816f1c7fc58061a8d4b99d210789fa3";
-  //   try {
-  //     const { data } = await axios.get(
-  //       `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`
-  //     );
-  //     setWeather(data)
-  //   } catch {
-  //     alert("Can't get data")
-  //   }
-  // };
 
-  // useEffect(() => {
-  //   navigator.geolocation.getCurrentPosition(
-  //     (position) => {
-  //       setLatitude(position.coords.latitude);
-  //       setLongitude(position.coords.longitude);
-  //     },
-  //     (error) => {
-  //       console.error(error);
-  //     }
-  //   );
-  //   longitude && latitude && getWeatherData(longitude, latitude)
-  // }, [longitude, latitude]);
 
-  // console.log(weather, longitude, latitude);
+
 
   return (
     <div>
